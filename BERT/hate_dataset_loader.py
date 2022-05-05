1bcd27d21a3a669fc3fc720887f0ea6c4dc80181
Ahmad-AlSubaie <ijustlikechocolate@gmail.com>
2022-05-04 22:02:57 -0400

    Trained better bert model

--- BERT/hate_dataset_loader.py
+++ BERT/hate_dataset_loader.py
@@ -8,7 +8,6 @@ def load_hate_dataset(return_type=None):
     train_dataset = train_dataset.map(lambda e: {"label" : int(np.median(e["annotators"]["label"]))})
     test_dataset = test_dataset.map(lambda e: {"label" : int(np.median(e["annotators"]["label"]))})
     val_dataset = val_dataset.map(lambda e: {"label" : int(np.median(e["annotators"]["label"]))})
-
     
     train_dataset = train_dataset.set_format(type=return_type, columns="post_tokens")
     test_dataset = test_dataset.set_format(type=return_type, columns="post_tokens")
